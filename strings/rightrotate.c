ce4394a1b5614e3f9b4742d781e793e5ec21a9e4
Debayan Nath <debayannath619@gmail.com>
2026-04-10 01:14:01 +0530

    Update rightrotate.c

--- strings/rightrotate.c
+++ strings/rightrotate.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
     char str[100], temp;
     int i, n;
-
     printf("Enter a string: ");
     gets(str);
     printf("Enter number of rotations: ");
     scanf("%d", &n);
-
     int len = strlen(str);
-
     while (n--) {
         temp = str[len - 1];
         for (i = len - 1; i > 0; i--) {
@@ -19,8 +15,6 @@ int main() {
         }
         str[0] = temp;
     }
-
     printf("String after right rotation: %s\n", str);
-
     return 0;
 }
